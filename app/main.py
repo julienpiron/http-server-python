ed18f13d50b4485a84cf7e348ebd7cc8b6afc5b1
Julien Piron <julien.piron@outlook.com>
2025-12-18 10:46:25 +0100

    feat: respond with 200

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
-import socket  # noqa: F401
+import socket
 
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    [client,_] = server_socket.accept() 
+    client.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    client.close()
 
 
 if __name__ == "__main__":
