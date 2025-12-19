8a66bb9648cfb7d8d00eb85a6622899d1ae7d6d9
Julien Piron <julien.piron@outlook.com>
2025-12-19 09:23:21 +0100

    feat: respond with body

--- app/main.py
+++ app/main.py
@@ -16,9 +16,14 @@ def main():
     if requestLine == None:
         return None
     url = requestLine.group("url")
+    if(not isinstance(url, str)):
+        raise Exception("Invalid URL")
 
     if(url == "/"):
         client.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    if(url.startswith("/echo/")):
+        payload = url[6:]
+        client.send(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(payload)}\r\n\r\n{payload}".encode("utf-8"))
     else:
         client.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
     client.close()
