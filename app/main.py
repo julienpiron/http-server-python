58d08d77bdf6030eb4f8227ac9e13bdf5d5e8aef
Julien Piron <julien.piron@outlook.com>
2025-12-19 08:58:32 +0100

    feat: extract url path

--- app/main.py
+++ app/main.py
@@ -1,10 +1,26 @@
 import socket
+import re
 
+PORT = 4221
+
+requestLineRegex = re.compile(r"^(?P<method>GET) (?P<url>[^\s]*)")
 
 def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket = socket.create_server(("localhost", PORT), reuse_port=True)
+    print(f"Listening on port {PORT}")
+
     [client,_] = server_socket.accept() 
-    client.send(b"HTTP/1.1 200 OK\r\n\r\n")
+
+    req = client.recv(1024).decode()
+    requestLine = requestLineRegex.match(req.splitlines()[0])
+    if requestLine == None:
+        return None
+    url = requestLine.group("url")
+
+    if(url == "/"):
+        client.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        client.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
     client.close()
 
 
